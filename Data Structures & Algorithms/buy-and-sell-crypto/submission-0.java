d44daf75c40b7256a0240eb71be294283d1d4664
yash ganesh shetty <yash22082004@gmail.com>
2026-08-31 19:08:38 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int left=0;
+        int right=1;
+        int maxp=0;
+        while(right<prices.length) {
+            if(prices[left]<prices[right]) {
+                int profit=prices[right]-prices[left];
+                maxp=Math.max(profit,maxp);
+                
+                right++;
+            }
+            else {
+                left=right;
+                right++;
+            }
+        }
+        return maxp;
+
+        
+    }
+}
