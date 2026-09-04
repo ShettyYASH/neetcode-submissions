2a5c0b3570b7ae3e19a541b50390adb56a588c05
yash ganesh shetty <yash22082004@gmail.com>
2026-09-04 05:41:59 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low=1;
+        int high=0;
+        for(int pile:piles){
+            high=Math.max(high,pile);
+        }
+        while(low<=high) {
+            int k=low+(high-low)/2;
+            int hours=0;
+            for(int pile:piles){
+                hours+=(pile+k-1)/k;
+            }
+            if(hours<=h) {
+                high=k-1;
+            }
+            else {
+                low=k+1;
+            }
+        }
+        return low;
+        
+    }
+}
